d5f92297fb60610f4cf79696b386ad223501049f
moncef aissaoui <108152453+modecode22@users.noreply.github.com>
2022-11-04 19:43:30 +0100

    just trying

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,27 @@ import React from 'react'
 import img from "../assets/Group.png"
 function Header() {
   return (
-    <div className='bg-slate-100 h-16 items-center shadow-black shadow-sm p-5 flex justify-between'>
-      <div className='flex '>
-        
+    <div className="bg-slate-100 h-16 items-center shadow-black shadow-sm p-5 flex justify-between">
+      <div className="flex group items-center justify-center gap-5">
+        <button className="z-10 relative shadow-sm shadow-black border-2 hover:shadow-none rounded-lg border-white bg-sky-500 bg flex items-center justify-center p-1">
+          <h1 className="z-10">إتصل بنا</h1>
+        </button>
+        <button className="shadow-sm shadow-black border-2 hover:shadow-none rounded-lg border-white bg-sky-500 flex items-center justify-center p-1">
+          من نحن؟
+        </button>
       </div>
-              <div className='relative peer z-10 cursor-pointer  shadow-sm shadow-black border-2 hover:shadow-none border-white bg-teal-500 w-12 h-12 rounded-full flex items-center justify-center group'>                 
-                <div className='absolute opacity-0 group-hover:opacity-100  duration-300  group-hover:translate-x-28 group-hover:scale-100 scale-[0.1] font-black text-3xl text-sky-500 w-full flex items-center'>المعالي </div>
-                <img src={img} className="w-12 group-hover:rotate-6 z-10 duration-300" alt="" />
-</div>    
+      <div className="relative peer z-10 cursor-pointer  shadow-sm shadow-black border-2 hover:shadow-none border-white bg-sky-500 w-12 h-12 rounded-full flex items-center justify-center group">
+        <div className="absolute opacity-0 group-hover:opacity-100  duration-300  group-hover:translate-x-28 group-hover:scale-100 scale-[0.1] font-black text-3xl text-sky-500 w-full flex items-center ">
+          المعالي{" "}
+        </div>
+        <img
+          src={img}
+          className="w-12 group-hover:rotate-6 z-10 duration-300"
+          alt=""
+        />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Header
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import img1 from "../assets/Group.png";
+import img from "../assets/back.png";
 
 function Hero() {
   return (
-    <div>
-      
+    <div className="h-96 flex justify-between px-20 ">
+      <div className="flex-col  items-center justify-center flex">
+        <h1 className=" font-black text-3xl animate-bounce mb-10  ">جمعية المعالي</h1>
+        <img className="w-80 h-56 animate-bounce " src={img1} alt="" />
+        <div className='w-60 h-1 shadow-sm shadow-black border-2 bg-sky-500 rounded-xl '></div>
+      </div>
+      <div className="z- 10">
+        <h1>المعالي</h1>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Hero
